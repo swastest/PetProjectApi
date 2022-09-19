923514aa8a02b2aa11238022e0f3f6b07019b468
Tatiana Kazakova <test.kazakova@gmail.com>
2022-09-19 17:12:51 +0300

    copy pet project

--- src/test/java/dataBase/DataBaseQuery.java
+++ src/test/java/dataBase/DataBaseQuery.java
@@ -13,13 +13,15 @@ public class DataBaseQuery {
 
     private int randomListSizeCity() {
         String sqlFindAllCity = "SELECT * FROM city where removed = false";
-        List<String> listSize = namedParameterJdbcTemplate.query(sqlFindAllCity, (rs, i) -> rs.getString("name"));
+        List<String> listSize = namedParameterJdbcTemplate.query(sqlFindAllCity,
+                (rs, i) -> rs.getString("name"));
         return listSize.size();
     }
 
     public String getRandomCity() {
         String sqlFindCity = "SELECT name FROM city where removed = false";
-        List<String> city = namedParameterJdbcTemplate.query(sqlFindCity, (rs, i) -> rs.getString("name"));
+        List<String> city = namedParameterJdbcTemplate.query(sqlFindCity,
+                (rs, i) -> rs.getString("name"));
         int max = randomListSizeCity();
         double rnd = Math.random() * max;
         int a = (int) rnd;
@@ -30,7 +32,8 @@ public class DataBaseQuery {
         String sqlFindPrice = "SELECT price_for_hour FROM city where removed = false and name = :cityName";
         Map<String, Object> map = new HashMap();
         map.put("cityName", city);
-        List<String> price = namedParameterJdbcTemplate.query(sqlFindPrice, map, (rs, i) -> rs.getString("price_for_hour"));
+        List<String> price = namedParameterJdbcTemplate.query(sqlFindPrice, map,
+                (rs, i) -> rs.getString("price_for_hour"));
         String a = price.get(0);
         //.split("\\.")[0];
         return Float.parseFloat(a);
@@ -40,7 +43,8 @@ public class DataBaseQuery {
         String sqlFindPrice = "SELECT id FROM city where removed = false and name = :cityName";
         Map<String, Object> map = new HashMap();
         map.put("cityName", city);
-        List<String> price = namedParameterJdbcTemplate.query(sqlFindPrice, map, (rs, i) -> rs.getString("id"));
+        List<String> price = namedParameterJdbcTemplate.query(sqlFindPrice, map,
+                (rs, i) -> rs.getString("id"));
         return Integer.parseInt(price.get(0));
     }
 
@@ -48,7 +52,8 @@ public class DataBaseQuery {
         String sqlFindLastScheduleId = "SELECT id FROM executor_schedule where user_id = :executorId Order by id desc LIMIT 1;";
         Map<String, Object> map = new HashMap<>();
         map.put("executorId", executorUserId);
-        List<Integer> scheduleId = namedParameterJdbcTemplate.query(sqlFindLastScheduleId, map, (rs, i) -> rs.getInt("id"));
+        List<Integer> scheduleId = namedParameterJdbcTemplate.query(sqlFindLastScheduleId,
+                map, (rs, i) -> rs.getInt("id"));
         return scheduleId.get(0);
     }
 
@@ -61,14 +66,16 @@ public class DataBaseQuery {
 
     public int howManyCountRegions(){
         String sqlFindCountRegions = "SELECT id FROM region where removed = false";
-        return namedParameterJdbcTemplate.query(sqlFindCountRegions,(rs, i) -> rs.getInt("id")).size();
+        return namedParameterJdbcTemplate.query(sqlFindCountRegions,
+                (rs, i) -> rs.getInt("id")).size();
     }
 
     public String clientAddress(int clientId, int index){
         String sqlSelectAddress = "SELECT  address FROM address where user_id = :clientId";
         Map<String,Integer> map = new HashMap<>();
         map.put("clientId", clientId);
-        List<String> s = namedParameterJdbcTemplate.query(sqlSelectAddress,map,(rs,i)->rs.getString("address"));
+        List<String> s = namedParameterJdbcTemplate.query(sqlSelectAddress,map,
+                (rs,i)->rs.getString("address"));
         return s.get(index);
     }
 
